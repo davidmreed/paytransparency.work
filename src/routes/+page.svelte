a041f226467660e60d57c986b7d4609eedb358ac
David Reed <david@ktema.org>
2023-02-20 17:46:28 -0700

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,14 @@
 </svelte:head>
 
 <h1>Discover Pay Transparency Laws</h1>
-<ul>
+<p>PayTransparency.work covers pay transparency laws in the United States. Discover pay transparency rules by state and city below. Not in an area with a disclosure law? You may still benefit from laws in states like California and Washington, which require multi-state employers to disclose in many situations.</p> 
+<div class="flex flex-row flex-wrap">
 	{#each locales as locale (locale.locale)}
-		<li><a href={`/locations/${locale.locale}`}>{#if locale.city}{locale.city}, {/if}{locale.state}</a></li>
+		<div class="basis-1/3 sm:basis-1/2">
+			let svgPath = `/svgs/${data.stateCode}.svg`;
+	<img alt={locale} src={svgPath} class="float-right" />
+
+			<a href={`/locations/${locale.locale}`}>{#if locale.city}{locale.city}, {/if}{locale.state}</a>
+		</div>
 	{/each}
-</ul>
+</div>
