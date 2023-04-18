d43f592a09754f9e3b02f99c04bd29716b38c96d
David Reed <david@ktema.org>
2023-04-18 10:49:34 -0600

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { inlineSvg } from '@svelte-put/inline-svg/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [vitePreprocess(), inlineSvg()],
+	
 	kit: {
 		adapter: adapter()
 	}
