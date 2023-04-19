e8df4d17be5a1db584fd196083c8a146842fb506
David Reed <david@ktema.org>
2023-04-19 17:48:20 -0600

    Map directory

--- src/lib/StateIcon.svelte
+++ src/lib/StateIcon.svelte
@@ -3,7 +3,7 @@
 	import type { Locale } from './data';
 
 	export let locale: Locale;
-	const svgPath = `/svgs/${locale.stateCode}.svg`;
+	const svgPath = `${locale.stateCode}.svg`;
 	const stateCode = locale.stateCode;
 </script>
 
@@ -11,103 +11,103 @@
 <!-- This ensures that paths are static at build time. -->
 
 {#if stateCode == 'AK'}
-<svg inline-src={"/svgs/AK.svg"} />
+<svg inline-src={"AK.svg"} />
 {:else if stateCode == 'AL'}
-<svg inline-src={"/svgs/AL.svg"} />
+<svg inline-src={"AL.svg"} />
 {:else if stateCode == 'AR'}
-<svg inline-src={"/svgs/AR.svg"} />
+<svg inline-src={"AR.svg"} />
 {:else if stateCode == 'AZ'}
-<svg inline-src={"/svgs/AZ.svg"} />
+<svg inline-src={"AZ.svg"} />
 {:else if stateCode == 'CA'}
-<svg inline-src={"/svgs/CA.svg"} />
+<svg inline-src={"CA.svg"} />
 {:else if stateCode == 'CO'}
-<svg inline-src={"/svgs/CO.svg"} />
+<svg inline-src={"CO.svg"} />
 {:else if stateCode == 'CT'}
-<svg inline-src={"/svgs/CT.svg"} />
+<svg inline-src={"CT.svg"} />
 {:else if stateCode == 'DC'}
-<svg inline-src={"/svgs/DC.svg"} />
+<svg inline-src={"DC.svg"} />
 {:else if stateCode == 'DE'}
-<svg inline-src={"/svgs/DE.svg"} />
+<svg inline-src={"DE.svg"} />
 {:else if stateCode == 'FL'}
-<svg inline-src={"/svgs/FL.svg"} />
+<svg inline-src={"FL.svg"} />
 {:else if stateCode == 'GA'}
-<svg inline-src={"/svgs/GA.svg"} />
+<svg inline-src={"GA.svg"} />
 {:else if stateCode == 'HI'}
-<svg inline-src={"/svgs/HI.svg"} />
+<svg inline-src={"HI.svg"} />
 {:else if stateCode == 'IA'}
-<svg inline-src={"/svgs/IA.svg"} />
+<svg inline-src={"IA.svg"} />
 {:else if stateCode == 'ID'}
-<svg inline-src={"/svgs/ID.svg"} />
+<svg inline-src={"ID.svg"} />
 {:else if stateCode == 'IL'}
-<svg inline-src={"/svgs/IN.svg"} />
+<svg inline-src={"IN.svg"} />
 {:else if stateCode == 'KS'}
-<svg inline-src={"/svgs/KS.svg"} />
+<svg inline-src={"KS.svg"} />
 {:else if stateCode == 'KY'}
-<svg inline-src={"/svgs/KY.svg"} />
+<svg inline-src={"KY.svg"} />
 {:else if stateCode == 'LA'}
-<svg inline-src={"/svgs/LA.svg"} />
+<svg inline-src={"LA.svg"} />
 {:else if stateCode == 'MA'}
-<svg inline-src={"/svgs/MA.svg"} />
+<svg inline-src={"MA.svg"} />
 {:else if stateCode == 'MD'}
-<svg inline-src={"/svgs/MD.svg"} />
+<svg inline-src={"MD.svg"} />
 {:else if stateCode == 'ME'}
-<svg inline-src={"/svgs/ME.svg"} />
+<svg inline-src={"ME.svg"} />
 {:else if stateCode == 'MI'}
-<svg inline-src={"/svgs/MI.svg"} />
+<svg inline-src={"MI.svg"} />
 {:else if stateCode == 'MN'}
-<svg inline-src={"/svgs/MN.svg"} />
+<svg inline-src={"MN.svg"} />
 {:else if stateCode == 'MO'}
-<svg inline-src={"/svgs/MO.svg"} />
+<svg inline-src={"MO.svg"} />
 {:else if stateCode == 'MS'}
-<svg inline-src={"/svgs/MS.svg"} />
+<svg inline-src={"MS.svg"} />
 {:else if stateCode == 'MT'}
-<svg inline-src={"/svgs/MT.svg"} />
+<svg inline-src={"MT.svg"} />
 {:else if stateCode == 'NC'}
-<svg inline-src={"/svgs/NC.svg"} />
+<svg inline-src={"NC.svg"} />
 {:else if stateCode == 'ND'}
-<svg inline-src={"/svgs/ND.svg"} />
+<svg inline-src={"ND.svg"} />
 {:else if stateCode == 'NE'}
-<svg inline-src={"/svgs/NE.svg"} />
+<svg inline-src={"NE.svg"} />
 {:else if stateCode == 'NH'}
-<svg inline-src={"/svgs/NH.svg"} />
+<svg inline-src={"NH.svg"} />
 {:else if stateCode == 'NJ'}
-<svg inline-src={"/svgs/NJ.svg"} />
+<svg inline-src={"NJ.svg"} />
 {:else if stateCode == 'NM'}
-<svg inline-src={"/svgs/NM.svg"} />
+<svg inline-src={"NM.svg"} />
 {:else if stateCode == 'NV'}
-<svg inline-src={"/svgs/NV.svg"} />
+<svg inline-src={"NV.svg"} />
 {:else if stateCode == 'NY'}
-<svg inline-src={"/svgs/NY.svg"} />
+<svg inline-src={"NY.svg"} />
 {:else if stateCode == 'OH'}
-<svg inline-src={"/svgs/OH.svg"} />
+<svg inline-src={"OH.svg"} />
 {:else if stateCode == 'OK'}
-<svg inline-src={"/svgs/OK.svg"} />
+<svg inline-src={"OK.svg"} />
 {:else if stateCode == 'OR'}
-<svg inline-src={"/svgs/OR.svg"} />
+<svg inline-src={"OR.svg"} />
 {:else if stateCode == 'PA'}
-<svg inline-src={"/svgs/PA.svg"} />
+<svg inline-src={"PA.svg"} />
 {:else if stateCode == 'RI'}
-<svg inline-src={"/svgs/RI.svg"} />
+<svg inline-src={"RI.svg"} />
 {:else if stateCode == 'SC'}
-<svg inline-src={"/svgs/SC.svg"} />
+<svg inline-src={"SC.svg"} />
 {:else if stateCode == 'SD'}
-<svg inline-src={"/svgs/SD.svg"} />
+<svg inline-src={"SD.svg"} />
 {:else if stateCode == 'TN'}
-<svg inline-src={"/svgs/TN.svg"} />
+<svg inline-src={"TN.svg"} />
 {:else if stateCode == 'TX'}
-<svg inline-src={"/svgs/TX.svg"} />
+<svg inline-src={"TX.svg"} />
 {:else if stateCode == 'UT'}
-<svg inline-src={"/svgs/UT.svg"} />
+<svg inline-src={"UT.svg"} />
 {:else if stateCode == 'VA'}
-<svg inline-src={"/svgs/VA.svg"} />
+<svg inline-src={"VA.svg"} />
 {:else if stateCode == 'VT'}
-<svg inline-src={"/svgs/VT.svg"} />
+<svg inline-src={"VT.svg"} />
 {:else if stateCode == 'WA'}
-<svg inline-src={"/svgs/WA.svg"} />
+<svg inline-src={"WA.svg"} />
 {:else if stateCode == 'WI'}
-<svg inline-src={"/svgs/WI.svg"} />
+<svg inline-src={"WI.svg"} />
 {:else if stateCode == 'WV'}
-<svg inline-src={"/svgs/WV.svg"} />
+<svg inline-src={"WV.svg"} />
 {:else if stateCode == 'WY'}
-<svg inline-src={"/svgs/WY.svg"} />
+<svg inline-src={"WY.svg"} />
 {/if}
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { inlineSvg } from '@svelte-put/inline-svg/preprocess';
+import { inlineSvg } from '@svelte-put/preprocess-inline-svg/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), inlineSvg()],
+	preprocess: [
+		inlineSvg([
+			{
+				directories: 'static/svgs',
+				attributes: {},
+			}
+	  ]),
+	  vitePreprocess()
+	],
 	
 	kit: {
 		adapter: adapter()
