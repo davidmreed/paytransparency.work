2655648aeed825b1fea0818aa5cc59b7d2f6747c
David Reed <david@ktema.org>
2024-12-14 12:21:05 -0700

    Move NPM install and caching into flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,14 @@
       let pkgs = import nixpkgs { system = system; config.allowUnfree = true; }; in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ nodejs_20 ];
+          packages = with pkgs; [ 
+            nodejs_20 
+            importNpmLock.hooks.linkNodeModulesHook  
+          ];
+          npmDeps = with pkgs; importNpmLock.buildNodeModules {
+            npmRoot = ./.;
+            nodejs = nodejs_20;
+          };
         };
       }
     );
